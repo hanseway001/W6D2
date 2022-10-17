d24b624b49d725a9ac9cd9dc37bc50fd0a6ddd6e
Wayne Hansen <whine24@gmail.com>
2022-10-17 16:15:15 -0600

    cleanup

--- script.js
+++ script.js
@@ -31,11 +31,5 @@ $('#fourSquareGame').append(
 )
 
 $('.gameBoard').on('click', '#row #button', function() {
-    // var id = $(this).attr('id');
-    // alert(id);
-    // alert($(this).children());
-//   $(this).parent().children('p').toggleClass('noDisplay');
-// $('#button').toggleClass('darker');
-    // $('#button').parent().children('#button').toggleClass('lighter');
     $(this).toggleClass('lighter');
-  });
+});
