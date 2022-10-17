45a82c0ef67312dabec086dabbba0d082f72e215
Wayne Hansen <whine24@gmail.com>
2022-10-17 16:13:42 -0600

    game complete

--- script.js
+++ script.js
@@ -1,30 +1,41 @@
 $('#fourSquareGame').append(
-    '<table class="gameBoard">' +
-    '<tr id="row">'+
-        '<td id="button"></td>'+
-        '<td id="button"></td>'+
-        '<td id="button"></td>'+
-        '<td id="button"></td>'+
-    '</tr>'+
-    '<tr id="row">'+
-        '<td id="button"></td>'+
-        '<td id="button"></td>'+
-        '<td id="button"></td>'+
-        '<td id="button"></td>'+
-    '</tr>'+
-    '<tr id="row">'+
-        '<td class="button"></td>'+
-        '<td id="button"></td>'+
-        '<td id="button"></td>'+
-        '<td id="button"></td>'+
-    '</tr>'+
-    '</table>'
-   
+
+    '<div class="gameBoard">' +
+    '<div id="row">'+
+        '<div id="button" class="darker"></div>'+
+        '<div id="button" class="darker"></div>'+
+        '<div id="button" class="darker"></div>'+
+        '<div id="button" class="darker"></div>'+
+    '</div>'+
+    '<div id="row">'+
+        '<div id="button" class="darker"></div>'+
+        '<div id="button" class="darker"></div>'+
+        '<div id="button" class="darker"></div>'+
+        '<div id="button" class="darker"></div>'+
+    '</div>'+
+    '<div id="row">'+
+        '<div id="button" class="darker"></div>'+
+        '<div id="button" class="darker"></div>'+
+        '<div id="button" class="darker"></div>'+
+        '<div id="button" class="darker"></div>'+
+    '</div>'+
+    '<div id="row">'+
+        '<div id="button" class="darker"></div>'+
+        '<div id="button" class="darker"></div>'+
+        '<div id="button" class="darker"></div>'+
+        '<div id="button" class="darker"></div>'+
+    '</div>'+
+    '</div>'
+
+
 )
 
-$('.gameBoard').on('click', '#row', function() {
-    // alert($(this).parent().children('p').text());
+$('.gameBoard').on('click', '#row #button', function() {
+    // var id = $(this).attr('id');
+    // alert(id);
+    // alert($(this).children());
 //   $(this).parent().children('p').toggleClass('noDisplay');
-$('#button').toggleClass('button');
-    $('#button').toggleClass('lighter');
+// $('#button').toggleClass('darker');
+    // $('#button').parent().children('#button').toggleClass('lighter');
+    $(this).toggleClass('lighter');
   });
